12daf6a39a85bfacc66de01943b84adfd0d56a6e
Kai Bansner <kai@bansner.com>
2026-03-25 11:00:55 -0700

    Add GA Event Tracking for Friend Finder Actions

--- src/js/main.js
+++ src/js/main.js
@@ -655,6 +655,13 @@ function createClassmateCard(classmateData) {
   emailBtn.innerHTML = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
   </svg>`;
+  emailBtn.addEventListener("click", function () {
+    // Send the event to Google Analytics
+    gtag("event", "button_click", {
+      button_name: "Email Them",
+      link_text: emailBtn.innerText,
+    });
+  });
   buttonContainer.appendChild(emailBtn);
 
   // SMS button (icon only)
@@ -667,6 +674,13 @@ function createClassmateCard(classmateData) {
   smsBtn.innerHTML = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
   </svg>`;
+  smsBtn.addEventListener("click", function () {
+    // Send the event to Google Analytics
+    gtag("event", "button_click", {
+      button_name: "Text Them",
+      link_text: smsBtn.innerText,
+    });
+  });
   buttonContainer.appendChild(smsBtn);
 
   // Share info link (icon only) - NOW WITH UID!
@@ -681,6 +695,13 @@ function createClassmateCard(classmateData) {
   shareLink.innerHTML = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
   </svg>`;
+  shareLink.addEventListener("click", function () {
+    // Send the event to Google Analytics
+    gtag("event", "button_click", {
+      link_name: "Share Their Info",
+      link_url: shareLink.href,
+    });
+  });
   buttonContainer.appendChild(shareLink);
 
   card.appendChild(buttonContainer);
